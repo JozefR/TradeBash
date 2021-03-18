904d7b6dafe546e2d2d91cb81290803b630d8663
Jozef Randjak <randjakjozef@gmail.com>
2021-03-18 21:07:48 +0100

    prototype for background service

--- TradeBash.DataCentre/Program.cs
+++ TradeBash.DataCentre/Program.cs
@@ -1,5 +1,12 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TradeBash.Core.Entities.Warehouse;
+using TradeBash.Infrastructure.Data.Repositories;
 
 namespace TradeBash.DataCentre
 {
@@ -12,6 +19,89 @@ namespace TradeBash.DataCentre
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<ConsumeScopedServiceHostedService>();
+                    services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
+                });
+    }
+
+    public class ConsumeScopedServiceHostedService : BackgroundService
+    {
+        private readonly ILogger<ConsumeScopedServiceHostedService> _logger;
+
+        public ConsumeScopedServiceHostedService(
+            IServiceProvider services,
+            ILogger<ConsumeScopedServiceHostedService> logger)
+        {
+            Services = services;
+            _logger = logger;
+        }
+
+        public IServiceProvider Services { get; }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service running.");
+
+            await DoWork(stoppingToken);
+        }
+
+        private async Task DoWork(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service is working.");
+
+
+            using (var scope = Services.CreateScope())
+            {
+                var scopedProcessingService = scope.ServiceProvider.GetRequiredService<IScopedProcessingService>();
+                var _stockRepository = scope.ServiceProvider.GetRequiredService<IStockRepository>();
+                var test = await _stockRepository.GetBySymbolAsync("aapl");
+
+                await scopedProcessingService.DoWork(stoppingToken);
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service is stopping.");
+
+            await base.StopAsync(stoppingToken);
+        }
+    }
+
+    internal interface IScopedProcessingService
+    {
+        Task DoWork(CancellationToken stoppingToken);
+    }
+
+    internal class ScopedProcessingService : IScopedProcessingService
+    {
+        private int executionCount = 0;
+        private readonly ILogger _logger;
+
+        public ScopedProcessingService(ILogger<ScopedProcessingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task DoWork(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                executionCount++;
+
+                _logger.LogInformation("Scoped Processing Service is working. Count: {Count}", executionCount);
+
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
     }
 }
--- TradeBash.DataCentre/Startup.cs
+++ TradeBash.DataCentre/Startup.cs
@@ -1,20 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TradeBash.Infrastructure;
+using TradeBash.Infrastructure.Data;
 using TradeBash.Infrastructure.Data.Repositories;
+using TradeBash.SharedKernel.Interfaces;
 
 namespace TradeBash.DataCentre
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IRepository, EfRepository>();
+            services.AddScoped<IStockRepository, StockRepository>();
+
+            services.AddDbContext(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
