e48c825561f691ccebfdfdb16f9bbe33810b33ff
Jozef Randjak <randjakjozef@gmail.com>
2021-05-02 13:14:00 +0200

    add lons sma parameter

---
+++ TradeBash/src/TradeBash.Infrastructure/Migrations/20210502111125_AddLongSMA.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradeBash.Infrastructure.Migrations
+{
+    public partial class AddLongSMA : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SMA",
+                table: "CalculatedStock");
+
+            migrationBuilder.AddColumn<double>(
+                name: "SMALong",
+                table: "CalculatedStock",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SMAShort",
+                table: "CalculatedStock",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SMALong",
+                table: "CalculatedStock");
+
+            migrationBuilder.DropColumn(
+                name: "SMAShort",
+                table: "CalculatedStock");
+
+            migrationBuilder.AddColumn<double>(
+                name: "SMA",
+                table: "CalculatedStock",
+                type: "float",
+                nullable: true);
+        }
+    }
+}
